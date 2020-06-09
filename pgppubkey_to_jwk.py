f75510e91cbe78e7085364d9a95c0f6a3266bc04
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-06-09 19:55:14 +0200

    Remove duplicated code

--- pgppubkey_to_jwk.py
+++ pgppubkey_to_jwk.py
@@ -1,17 +1,9 @@
-from authlib.common.encoding import int_to_base64
 import json
-import pgpy
+from josepy import JWK
 
+with open('public_release.pem', 'rb') as f:
+    key = f.read()
 
-# Input is PGP armored pubkey
-key, _ = pgpy.PGPKey.from_file('key.asc')
-n = int(key._key.keymaterial.n)
-e = int(key._key.keymaterial.e)
-
-# Expected JWK format according to https://tools.ietf.org/html/rfc7518#section-6.1
-pubkey = {
-    'kty': 'RSA',
-    'e': int_to_base64(e),
-    'n': int_to_base64(n)
-}
-print(json.dumps(pubkey))
+jwk = JWK.load(key).fields_to_partial_json()
+jwk['kty'] = 'RSA'
+print(json.dumps(jwk, indent=4, sort_keys=True))
